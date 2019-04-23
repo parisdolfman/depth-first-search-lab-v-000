00b3965d23022548098b364ea658c3e7730b9159
Paris Dolfman <parisdolfman@gmail.com>
2019-04-23 18:17:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function depthFirstSearch(rootNode, vertices, edges) {
+  let stack = [rootNode]
+  let visited = []
+  visited.push(rootNode)
+  while (stack.length > 0) {
+    let node = stack.pop();
+      if (node.discovered) continue;
+        node.discovered = true;
+        let adjacent = findAdjacent(node.name, vertices, edges)
+        adjacent.map(vertex => {visited.push(vertex); stack.push(vertex);
+        });
+        }
+  return visited
+}
+
+function findAdjacent(name, vertices, edges) {
+  let adjacent = edges.filter(edge => edge.includes(name))
+                    .map(edge => edge.filter(v => v != name)) //remove self
+                    .map(v => v[0]); // flatten
+
+  return vertices.filter(vertex =>
+                              adjacent.includes(vertex.name) &&
+                              vertex.discovered === null);
+}
